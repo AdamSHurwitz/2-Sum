aec3350456a17346741cb6eb397a641cd5e16e99
Adam Hurwitz <adamscotthurwitz@gmail.com>
2018-01-29 01:02:49 -0800

    solved

--- src/TwoSum.kt
+++ src/TwoSum.kt
@@ -1,8 +1,25 @@
 import java.util.*
+import kotlin.collections.HashSet
 
 class TwoSum {
-    fun calculate(hashTable: Hashtable<Int, Boolean>){
+    fun calculate(hashSet: HashSet<Long>, targetRange: Pair<Int, Int>): Int {
         var count = 0;
-
+        var targetRangeList = arrayListOf<Long>()
+        for (t in targetRange.first..targetRange.second) {
+            targetRangeList.add(t.toLong())
+        }
+        for (x in hashSet) {
+            var targetIndex = 0;
+            while (targetIndex < targetRangeList.size) {
+                var y = targetRangeList[targetIndex] - x
+                if (y != x && hashSet.contains(y)) {
+                    targetRangeList.removeAt(targetIndex)
+                    count++
+                } else {
+                    targetIndex++
+                }
+            }
+        }
+        return count
     }
 }
--- src/TwoSumTest.kt
+++ src/TwoSumTest.kt
@@ -1,9 +1,18 @@
+import java.io.File
 import java.util.*
 
-object TwoSumTest{
+object TwoSumTest {
     @JvmStatic
-    fun main(args: Array<String>){
-        var hashTable = Hashtable<Int, Boolean>();
-        TwoSum().calculate(hashTable)
+    fun main(args: Array<String>) {
+        var hashSet = HashSet<Long>()
+        File("src/data/dataSet.txt")
+                .inputStream()
+                .bufferedReader()
+                .useLines { lines ->
+                    lines.forEach {
+                        hashSet.add(it.toLong())
+                    }
+                }
+        print("Number of targets with addents in the data set: " + TwoSum().calculate(hashSet, targetRange = Pair(-10000, 10000)))
     }
 }
